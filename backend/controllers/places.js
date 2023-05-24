8b2d4a513b60fb9fa7ab98e9e8913d6808b81dce
GreggLedoux <guitlouie@gmail.com>
2023-05-24 19:23:16 -0400

    JWT is not Jawt

--- backend/controllers/places.js
+++ backend/controllers/places.js
@@ -106,6 +106,51 @@ router.post('/:placeId/comments', async (req, res) => {
         res.status(404).json({ message: `Could not find place with id "${placeId}"` })
     }
 
+    if (!req.currentUser) {
+        return res.status(404).json({ message: `You must be logged in to leave a rant or rave.` })
+    }
+
+    const comment = await Comment.create({
+        ...req.body,
+        authorId: req.currentUser.userId,
+        placeId: placeId
+    })
+
+    res.send({
+        ...comment.toJSON(),
+        author: req.currentUser
+    })
+})
+
+
+    
+    let currentUser;
+    try {
+        const [method, token] = req.headers.authorization.split(' ')
+        if (method == 'Bearer') {
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+            const { id } = result.value
+            currentUser = await User.findOne({
+                where: {
+                    userId: id 
+                }
+            })
+        }
+    } catch {
+        currentUser = null
+    }
+
+    if (!currentUser) {
+        return res.status(404).json({
+            message: `You must be logged in to leave a rant or rave.`
+        })
+    }
+
+
+   
+
+
+
     const author = await User.findOne({
         where: { userId: req.body.authorId }
     })
@@ -114,37 +159,39 @@ router.post('/:placeId/comments', async (req, res) => {
         res.status(404).json({ message: `Could not find author with id "${req.body.authorId}"` })
     }
 
-    const comment = await Comment.create({
-        ...req.body,
-        placeId: placeId
-    })
 
-    res.send({
-        ...comment.toJSON(),
-        author
-    })
-})
 
-router.delete('/:placeId/comments/:commentId', async (req, res) => {
-    let placeId = Number(req.params.placeId)
-    let commentId = Number(req.params.commentId)
+  
 
-    if (isNaN(placeId)) {
-        res.status(404).json({ message: `Invalid id "${placeId}"` })
-    } else if (isNaN(commentId)) {
-        res.status(404).json({ message: `Invalid id "${commentId}"` })
-    } else {
-        const comment = await Comment.findOne({
-            where: { commentId: commentId, placeId: placeId }
-        })
-        if (!comment) {
-            res.status(404).json({ message: `Could not find comment with id "${commentId}" for place with id "${placeId}"` })
+    router.delete('/:placeId/comments/:commentId', async (req, res) => {
+        let placeId = Number(req.params.placeId)
+        let commentId = Number(req.params.commentId)
+    
+        if (isNaN(placeId)) {
+            res.status(404).json({ message: `Invalid id "${placeId}"` })
+        } else if (isNaN(commentId)) {
+            res.status(404).json({ message: `Invalid id "${commentId}"` })
         } else {
-            await comment.destroy()
-            res.json(comment)
+            const comment = await Comment.findOne({
+                where: { commentId: commentId, placeId: placeId }
+            })
+            if (!comment) {
+                res.status(404).json({ 
+                    message: `Could not find comment` 
+                })
+            } else if (comment.authorId !== req.currentUser?.userId) {
+                res.status(403).json({ 
+                    message: `You do not have permission to delete comment "${comment.commentId}"` 
+                })
+            } else {
+                await comment.destroy()
+                res.json(comment)
+            }
         }
-    }
-})
+    })
+    
+      
+    
 
 
 module.exports = router
